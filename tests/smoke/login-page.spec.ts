524deee36087105454282e3e3f54e0d4751bb599
Alejandro <agp901@gmail.com>
2025-09-25 22:30:14 +0200

    [test-login]: remove unnecessary code after refactor

--- tests/smoke/login-page.spec.ts
+++ tests/smoke/login-page.spec.ts
@@ -1,8 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/login-page";
 
-test.describe.configure({ mode: "serial" });
-
 test.describe("@Smoke - Login Page", () => {
   test("Login Modal UI elements is visible", async ({page}) => {
     const loginPage: LoginPage = new LoginPage(page);
@@ -16,7 +14,6 @@ test.describe("@Smoke - Login Page", () => {
     });
     await test.step("Password input is visible", async () => {
       await expect(loginPage.passwordField).toBeVisible();
-      await loginPage.passwordField.fill('1234')
     });
     await test.step("Access button is visible", async () => {
       await expect(loginPage.loginButton).toBeVisible();
