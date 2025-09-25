f837707aef8eded9ef6de954b26d4c220f36102e
Alejandro <agp901@gmail.com>
2025-09-25 19:17:40 +0200

    [test-login]: Configure Smoke Test in serial mode, for improve the performance

--- tests/smoke/login-page.spec.ts
+++ tests/smoke/login-page.spec.ts
@@ -1,30 +1,39 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 import { LoginPage } from "../../pages/login-page";
 
-let loginPage: LoginPage;
+test.describe.configure({ mode: "serial" });
 
-test.beforeEach(async ({ page }) => {
+test.describe("@Smoke - Login Page", () => {
+  let loginPage: LoginPage;
+  let page: Page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
     loginPage = new LoginPage(page);
     await loginPage.openLoginModal();
-});
+  });
 
-test.describe('@Smoke - Login Page', () => {
-    test('When user Click to "ACCESS" login modal is open', async () => {
-        await expect(loginPage.loginComponent).toBeVisible();
-    });
-    test('User/Email input is visible', async () => {
-        await expect(loginPage.userField).toBeVisible();
-    });
-    test('Password input is visible', async () => {
-        await expect(loginPage.passwordField).toBeVisible();
-    });
-    test('Access button is visible', async () => {
-        await expect(loginPage.loginButton).toBeVisible();
-    });
-    test('Register button is visible', async () => {
-        await expect(loginPage.registerButton).toBeVisible();
-    });
-    test('Close Login Modal is visible', async () => {
-        await expect(loginPage.closeButton).toBeVisible();
-    });           
-})
+  test.afterAll(async () => {
+    await page.close();
+  });
+
+  // TESTS
+  test('When user Click to "ACCESS" login modal is open', async ({ page }) => {
+    await expect(loginPage.loginComponent).toBeVisible();
+  });
+  test("User/Email input is visible", async () => {
+    await expect(loginPage.userField).toBeVisible();
+  });
+  test("Password input is visible", async () => {
+    await expect(loginPage.passwordField).toBeVisible();
+  });
+  test("Access button is visible", async () => {
+    await expect(loginPage.loginButton).toBeVisible();
+  });
+  test("Register button is visible", async () => {
+    await expect(loginPage.registerButton).toBeVisible();
+  });
+  test("Close Login Modal is visible", async () => {
+    await expect(loginPage.closeButton).toBeVisible();
+  });
+});
