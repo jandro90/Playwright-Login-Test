db55b0404aa55b82d5fb6ab07df439b77811983f
Alejandro <agp901@gmail.com>
2025-09-26 08:17:00 +0200

    [test-login]: comment extra devices projects in dev config environment

--- configs/dev.config.ts
+++ configs/dev.config.ts
@@ -1,45 +1,45 @@
 // e2e/configs/dev.config.ts
-import { defineConfig, devices } from '@playwright/test';
-import baseConfig from '../playwright.config';
+import { defineConfig, devices } from "@playwright/test";
+import baseConfig from "../playwright.config";
 
-const baseURL = 'https://m.apuestas.codere.es/';
+const baseURL = "https://m.apuestas.codere.es/";
 
 export default defineConfig({
   ...baseConfig,
-  testDir: '../tests',
-  outputDir: '../test-results',
+  testDir: "../tests",
+  outputDir: "../test-results",
   workers: 1,
   use: {
     baseURL,
     headless: false,
-    screenshot: 'only-on-failure',
-    trace: 'retain-on-failure',
-    video: 'retain-on-failure',
+    screenshot: "only-on-failure",
+    trace: "retain-on-failure",
+    video: "retain-on-failure",
   },
 
   projects: [
-/*     DESKTOP PROJECTS */
-  {
-      name: 'Chrome',
-      use: { ...devices['Desktop Chrome'] },
-  },
-  {
-     name: 'Firefox',
-     use: { ...devices['Desktop Firefox'] },
-   },
+    /*     DESKTOP PROJECTS */
+    {
+      name: "Chrome",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    // {
+    //   name: "Firefox",
+    //   use: { ...devices["Desktop Firefox"] },
+    // },
 
-   {
-     name: 'Webkit',
-     use: { ...devices['Desktop Safari'] },
-   },
-   /*  MOBILE PROJECTS */
-   {
-     name: 'IOS',
-     use: { ...devices['iPhone 12 Pro'] },
-   },
-   {
-     name: 'ANDROID',
-     use: { ...devices['Galaxy S24'] },
-   },
+    // {
+    //   name: "Webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
+    /*  MOBILE PROJECTS */
+    // {
+    //   name: "IOS",
+    //   use: { ...devices["iPhone 12 Pro"] },
+    // },
+    // {
+    //   name: "ANDROID",
+    //   use: { ...devices["Galaxy S24"] },
+    // },
   ],
 });
